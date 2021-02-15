d75dfbf66edb23605138bd87ccfef2fd7feed0a1
anwarshraideh <ahshraida@gmail.com>
2021-02-15 16:23:41 +0200

    complete the task

---
+++ demo/js/app.js
@@ -0,0 +1,75 @@
+'use strict'
+
+let userName =prompt("whats your name ?");
+alert ("welcome " + userName + " :)");
+
+let qus1 =prompt("Do i like roses ? (yes/no , y/n) ");
+//console.log("you answerd " + qus1);
+
+if (qus1.toLowerCase() === 'yes' || qus1.toUpperCase() =='YES' || qus1.toUpperCase() =='Y' || qus1.toLowerCase() =='y'  ){
+    alert("You are right, I love roses");
+} else if (qus1.toLowerCase() === 'no' || qus1.toUpperCase() =='NO' ||qus1.toLowerCase() === 'n' || qus1.toUpperCase() =='N' ) {
+    alert("You are wrong ,why do you think that ?");
+}
+else
+{
+    alert("you didn't answer in yes or no ");
+
+}
+
+let qus2 =prompt("Do i like spend time at home ? (yes/no , y/n)");
+//console.log("you answerd " + qus2);
+if (qus2.toLowerCase() === 'yes' || qus2.toUpperCase() =='YES' || qus2.toUpperCase() =='Y' || qus2.toLowerCase() =='y'  ){
+    alert("You are right ");
+} else if (qus2.toLowerCase() === 'no' || qus2.toUpperCase() =='NO' ||qus2.toLowerCase() === 'n' || qus2.toUpperCase() =='N' ) {
+    alert("You are wrong ,why do you think that ?");
+}
+else
+{
+    alert("you didn't answer in yes or no ");
+
+}
+
+let qus3 =prompt("Do you think i like a cars ? (yes/no , y/n)");
+
+//console.log("you answerd " + qus3);
+if (qus3.toLowerCase() === 'yes' || qus3.toUpperCase() =='YES' || qus3.toUpperCase() =='Y' || qus3.toLowerCase() =='y'  ){
+    alert("You are wrong ");
+} else if (qus3.toLowerCase() === 'no' || qus3.toUpperCase() =='NO' ||qus3.toLowerCase() === 'n' || qus3.toUpperCase() =='N' ) {
+    alert("You are right i dont love cares ");
+}
+else
+{
+    alert("you didn't answer in yes or no ");
+
+}
+
+let qus4 =prompt("Do you think i love sleeping ? (yes/no , y/n)");
+//console.log("you answerd " + qus4);
+if (qus4.toLowerCase() === 'yes' || qus4.toUpperCase() =='YES' || qus4.toUpperCase() =='Y' || qus4.toLowerCase() =='y'  ){
+    alert("You are right, i think that sleeping is the best thing in the world  ");
+} else if (qus4.toLowerCase() === 'no' || qus4.toUpperCase() =='NO' ||qus4.toLowerCase() === 'n' || qus4.toUpperCase() =='N' ) {
+    alert("You are wrong ,why do you think that ?");
+}
+else
+{
+    alert("you didn't answer in yes or no ");
+
+}
+
+let qus5 =prompt("Am i a serious person ? (yes/no , y/n)");
+//console.log("you answerd " + qus5);
+if (qus5.toLowerCase() === 'yes' || qus5.toUpperCase() =='YES' || qus5.toUpperCase() =='Y' || qus5.toLowerCase() =='y'  ){
+    alert("wrong, iam just a queit person ");
+} else if (qus5.toLowerCase() === 'no' || qus5.toUpperCase() =='NO' ||qus5.toLowerCase() === 'n' || qus5.toUpperCase() =='N' ) {
+    alert("You are right");
+}
+else
+{
+    alert("you didn't answer in yes or no ");
+
+}
+
+
+
+
